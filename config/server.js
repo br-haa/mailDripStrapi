37a3fd00498488de70a2a89042a2b61c91eda6a8
Benjamin <work@benjaminrussell.me>
2021-03-23 15:52:08 -0500

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '62e51506fdad6d7912656da3629801fe'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dcc3d284-a5ab-4818-a2f5-057c6e254e3d'
+};
